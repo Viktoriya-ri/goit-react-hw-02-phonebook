9fcdd4e69e0d372bce4d01e0c3be126d367d1900
Вікторія <viktoriya23071@gmail.com>
2023-09-21 19:23:43 +0300

    npm

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+
+state = {
+  contacts: [],
+  name: ''
+}
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,5 @@
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import { FormWrapper, FormInput, Button, Error } from './Input.styled';
+
+const schema = 
