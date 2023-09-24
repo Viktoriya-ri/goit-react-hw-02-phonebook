cb51be4953e9001b68f72f84f99cf5b863336e48
Вікторія <viktoriya23071@gmail.com>
2023-09-24 21:01:43 +0300

    s

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,23 +1,26 @@
-import { Formik } from 'formik';
-import { Component } from 'react';
-import { Form, Input, Button, Text } from './Input.styled';
-export default class ImputForm extends Component {
-  state = {
-    contacts: [],
+import { Formik, Form } from 'formik';
+import * as yup from 'yup';
+import { FormWrapper, FormInput, Button, Error } from './Input.styled';
+
+const schema = yup.object().shape({
+  name: yup.string().min(3).max(30).required(),
+  number: yup.number().min(3).required(),
+});
+
+const Input = ({ onsubmit }) => {
+
+  const initialValues = {
     name: '',
     number: '',
-  };
-  handleInputChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  };
-  
-  render() {
-    <form>
-      <h2>Phonebook</h2>
-    </form>
   }
+  return( {
+  <Formik initialValues={initialValues} validationSchema={schema} onSubmit={onSubmit}>
+    <FormWrapper>
+      <FormInput type='text' name='name' ></FormInput>
+
+      </FormWrapper>
+    </Formik>
+  })
 
 }
 
