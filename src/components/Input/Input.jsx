147919932540d7598f878623b7c83838daabd67a
Вікторія <viktoriya23071@gmail.com>
2023-09-24 23:59:28 +0300

    Update Input.jsx

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import { FormWrapper, FormInput, Button, Error } from './Input.styled';
