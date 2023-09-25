99d9b01e040b5133aa9c24fe07cf0cce3a3febbc
Вікторія <viktoriya23071@gmail.com>
2023-09-25 21:44:23 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -65,20 +65,18 @@ class ContactForm extends Component {
         <Input
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' \\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
-          value={this.state.name}
+          value={name}
           onChange={this.handleNameChange}
         />
         <Text>Number</Text>
         <Input
           type="tel"
           name="number"
-          pattern="\\+?\\d{1,4}?[ .\\-\\s]?\\(?\\d{1,3}?\\)?[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
-          value={this.state.number}
+          value={number}
           onChange={this.handleNumberChange}
         />
         <Button type="submit">Add Contact</Button>
